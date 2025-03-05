3c28893d8878390460a5a340d395c6dbd41b24ed
Pilaucodes <shelsmith.ilusa02@gmail.com>
2025-03-05 15:26:46 +0100

    first commit

---
+++ eclipse-workspace/appointment_booking_system/src/appointment_booking_system/AppointmentBookingSystem.java
@@ -0,0 +1,10 @@
+package appointment_booking_system;
+
+public class AppointmentBookingSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ eclipse-workspace/appointment_booking_system/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module appointment_booking_system {
+}
