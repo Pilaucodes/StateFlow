4c101ec7464c7b51a3ce46ed3e97f061fb4a8f3c
Pilaucodes <you@example.com>
2026-07-24 21:07:37 +0200

    dashboard user interface layout.

--- Desktop/Stateflow/stateflow/src/ui/Dashboard/Dashboard.jsx
+++ Desktop/Stateflow/stateflow/src/ui/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import "./Dashboard.css";
 // reusable statistic component import
 import Statistic from "../components/Statistic";
 
-//display main dashboard with statistics and other dashboard widgets
+//displays main dashboard with statistics and other dashboard widgets
 function Dashboard() {
   return (
     <div className="dashboard">
@@ -37,8 +37,59 @@ function Dashboard() {
 
       </div>
 
+ // Chart sectioon 
+      <div className="dashboard-charts">
+
+        <div className="chart-card">
+          <h2>Task Completion Progress</h2>
+          <p>...</p>
+        </div>
+
+        <div className="chart-card">
+          <h2>Tasks by Priority</h2>
+          <p>...</p>
+        </div>
+
+        <div className="chart-card">
+          <h2>Focus Sessions This Week</h2>
+          <p>...</p>
+        </div>
+
+      </div>
+
+      //lower dash-area
+      <div className="dashboard-bottom">
+
+        // recent tasks
+
+        <div className="recent-tasks">
+
+          <h2>Recent Tasks</h2>
+
+          <ul>
+            <li>Fix Login API</li>
+            <li>Implement Kanban Board</li>
+            <li>Update Database Schema</li>
+          </ul>
+
+        </div>
+
+        // quick-actions
+
+        <div className="quick-actions">
+
+          <h2>Quick Actions</h2>
+
+          <button>Create Task</button>
+          <button>Start Focus Session</button>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
 
 export default Dashboard;
+ 
